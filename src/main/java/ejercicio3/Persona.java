4153cf8c4d3a97e7ac7c62cfcf3145581e30d988
tomas_gnzlz <tgonati327@g.educaand.es>
2023-02-11 13:57:47 +0100

    Ejercicio 3

---
+++ src/main/java/ejercicio3/Persona.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio3;
+
+/**
+ *
+ * @author tomas
+ */
+public class Persona {
+    
+    private String nombre;
+    private String nif;
+    private String edad;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String nif, String edad) {
+        this.nombre = nombre;
+        this.nif = nif;
+        this.edad = edad;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "Nombre=" + nombre + ", NIF=" + nif + ", Edad=" + edad + '}';
+    }
+    
+    
+}
---
+++ src/main/java/ejercicio3/Programador.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio3;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Programador extends Empleado {
+
+    CategoriasProgramador categoria;
+
+    public Programador() {
+    }
+
+    public Programador(CategoriasProgramador categoria, int salario, String nombre, String nif, String edad) {
+        super(salario, nombre, nif, edad);
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " || " + "Programador{" + "categoria=" + categoria + '}';
+    }
+}
