e25e06094a50ab7e207ca3a692fc0f1ebdf577c4
tomas_gnzlz <tgonati327@g.educaand.es>
2023-02-14 21:42:04 +0100

    -

--- src/main/java/repasoestructurasalmacenamiento/Ejercicio1.java
+++ src/main/java/repasoestructurasalmacenamiento/Ejercicio1.java
@@ -5,8 +5,12 @@
 package repasoestructurasalmacenamiento;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  *
@@ -16,34 +20,12 @@ public class Ejercicio1 {
 
     public static void main(String[] args) {
 
-        int n = pedirDimension();
+        int n = 3;
         int[][] matriz = new int[n][n];
-        rellenarMatriz(n, matriz);
+        rellenarMatrizSinRepetirValores(n, matriz);
         mostrarMatriz(matriz);
 
-    }
-
-    // Método para pedir las dimensiones.
-    public static int pedirDimension() {
-        int n = 0;
-        Scanner teclado = new Scanner(System.in);
-        System.out.println("Introduce las dimensiones de la matriz.");
-        n = teclado.nextInt();
-        return n;
-    }
-
-    // Método para rellenar una matriz con valores de un array.
-    public static void rellenarMatriz(int n, int[][] matrizAux) {
-        int nValores = (n * n);
-        Random random = new Random();
-        random.nextInt(nValores);
-        for (int i = 0; i < matrizAux.length; i++) {
-            System.out.println(" ");
-            for (int j = 0; j < matrizAux.length; j++) {
-                matrizAux[i][j] = random.nextInt(nValores) + 1;
-                // COMO HACER PARA QUE NO SE REPITAN LOS VALORES. 
-            }
-        }
+        // NO SE COMO HACER PARA QUE NO SE REPITAN LOS VALORES. 
     }
 
     public static void mostrarMatriz(int[][] matrizAux) {
@@ -52,6 +34,46 @@ public class Ejercicio1 {
             System.out.println(" ");
             for (int j = 0; j < matrizAux.length; j++) {
                 System.out.print(matrizAux[i][j] + " ");
+            }
+        }
+    }
+
+////    public static void RellenarMatriz(int n, int[][] matrizAux) {
+////        Random random = new Random();
+////        int nValores = (n * n);
+////        for (int i = 0; i < matrizAux.length; i++) {
+////            System.out.println(" ");
+////            for (int j = 0; j < matrizAux.length; j++) {
+////                matrizAux[i][j] = random.nextInt(1, nValores) + 1;
+////                if (true) {
+////
+////                }
+////            }
+////        }
+////    }
+    public static void rellenarMatrizSinRepetirValores(int n, int[][] matrizAux) {
+        Random random = new Random();
+        int nValores = (n * n);
+        //Creo un Array y lo relleno con valores desde (1-n*n).
+        int[] ArrayValores = new int[nValores];
+        for (int i = 0; i < ArrayValores.length; i++) {
+            ArrayValores[i] = i + 1;
+        }
+        // COMO QUITO EL ORDEN A LOS ELEMENTOS DE ARRAY.
+        
+        
+
+        // Una vez tengo rellenado el array, procedo a rellenar la matriz con dichos valores. 
+        int Aux = 0;
+        for (int i = 0; i < matrizAux.length; i++) {
+            System.out.println(" ");
+            for (int j = 0; j < matrizAux.length; j++) {
+                if (Aux < ArrayValores.length) {
+                    matrizAux[i][j] = ArrayValores[Aux];
+                    Aux++;
+                } else {
+                    return;
+                }
             }
         }
     }
--- src/main/java/repasoestructurasalmacenamiento/Ejercicio2.java
+++ src/main/java/repasoestructurasalmacenamiento/Ejercicio2.java
@@ -11,24 +11,18 @@ import java.util.Scanner;
  *
  * @author tomas
  */
-public class Ejercicio2 {public static void main(String[] args) {
+public class Ejercicio2 {
 
-        int n = pedirDimension();
+    public static void main(String[] args) {
+
+        int n = 3;
         int[][] matriz = new int[n][n];
         rellenarMatrizRandom(matriz);
+        System.out.println("DIMENSIÓN:        3");
         System.out.println("            MATRIZ CREADA");
         mostrarMatriz(matriz);
         // LO QUE SIGUE...
 
-    }
-
-    // Método para pedir las dimensiones.
-    public static int pedirDimension() {
-        int n = 0;
-        Scanner teclado = new Scanner(System.in);
-        System.out.println("Introduce las dimensiones de la matriz.");
-        n = teclado.nextInt();
-        return n;
     }
 
     //Método para rellenar la matriz de 0 y 1. 
@@ -36,11 +30,10 @@ public class Ejercicio2 {public static void main(String[] args) {
         Random random = new Random();
         for (int i = 0; i < matrizAux.length; i++) {
             for (int j = 0; j < matrizAux.length; j++) {
-                 matrizAux[i][j] = (int) (Math.random() * 2);
+                matrizAux[i][j] = (int) (Math.random() * 2);
             }
         }
     }
-    
 
     //Método para mostrar la matriz creada.
     public static void mostrarMatriz(int[][] matrizAux) {
@@ -53,4 +46,10 @@ public class Ejercicio2 {public static void main(String[] args) {
         }
     }
     
+    // Método que comprueba las esquinas de aquellas coordenadas donde se encuentra un 0. 
+    // si todas tienen 1 ese cero ese es el que buscamos.
+    public static void comprobarEsquinasCeros(){
+        
+    }
+
 }
