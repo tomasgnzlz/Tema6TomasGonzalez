28c9aa1678972f8db96eccb1af3450598bc2f301
tomasgnzlz <tgonati327@g.educaand.es>
2023-02-15 14:38:16 +0100

    Ejercicios repasio

--- src/main/java/repasoestructurasalmacenamiento/Ejercicio1.java
+++ src/main/java/repasoestructurasalmacenamiento/Ejercicio1.java
@@ -4,10 +4,11 @@
  */
 package repasoestructurasalmacenamiento;
 
-import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
@@ -20,16 +21,13 @@ public class Ejercicio1 {
 
     public static void main(String[] args) {
 
-        int n = 3;
-        int[][] matriz = new int[n][n];
-        rellenarMatrizSinRepetirValores(n, matriz);
+        int n = 3; // Le paso las dimensiones de la matriz. 
+        int[][] matriz = new int[n][n]; // La matriz siempre será cuadrada.
+        rellenarMatrizAleatoriamente(n, matriz);
         mostrarMatriz(matriz);
-
-        // NO SE COMO HACER PARA QUE NO SE REPITAN LOS VALORES. 
     }
 
     public static void mostrarMatriz(int[][] matrizAux) {
-
         for (int i = 0; i < matrizAux.length; i++) {
             System.out.println(" ");
             for (int j = 0; j < matrizAux.length; j++) {
@@ -38,6 +36,84 @@ public class Ejercicio1 {
         }
     }
 
+    // Método para rellenar
+    public static void rellenarMatrizAleatoriamente(int n, int[][] matrizAux) {
+        Random random = new Random();
+        int numValores = (n * n);
+
+        // Creo una lista y la relleno con valores de 1 a n*n.
+        List<Integer> listaNum = new ArrayList<Integer>();
+        for (int i = 1; i <= numValores; i++) {
+            listaNum.add(i);
+        }
+        System.out.println("Creo lista valores de 1 a n*n \n" + listaNum + "\n");
+        // Utilizo el método shuffle de la clase collecions para desordenar los valores que se encuentran en la lista.
+        Collections.shuffle(listaNum);
+        System.out.println("Lista desordenada: \n" + listaNum + "\n");
+
+        //Ahora relleno la matriz con los valores de la lista. 
+        System.out.println("        MATRIZ CON NUMEROS ALEATORIOS");
+        int contador = 0;
+        /*
+        La variable contador la meto para poder saber en que posicion de la lista me encuentro,
+        si no pongo el contador no podría posiiconarme en la lista, y por ende, no podría rellenar la matriz. 
+         */
+        for (int i = 0; i < matrizAux.length; i++) {
+            for (int j = 0; j < matrizAux.length; j++) {
+                matrizAux[i][j] = listaNum.get(contador);
+                contador++;
+                /*
+                Si no voy incrementando el valor del contador, estaría rellenando la matriz, 
+                con el primer valor de la lista. En cambio, si lo voy incrementando, 
+                en cada iteración se coloca en la siguiente posicion en la lista, 
+                y mete el valor de esa posición en la matriz. 
+                 */
+            }
+        }
+    }
+
+    /*
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+     */
+    // METOFOS QUE NO VALEN
 ////    public static void RellenarMatriz(int n, int[][] matrizAux) {
 ////        Random random = new Random();
 ////        int nValores = (n * n);
@@ -51,30 +127,30 @@ public class Ejercicio1 {
 ////            }
 ////        }
 ////    }
-    public static void rellenarMatrizSinRepetirValores(int n, int[][] matrizAux) {
-        Random random = new Random();
-        int nValores = (n * n);
-        //Creo un Array y lo relleno con valores desde (1-n*n).
-        int[] ArrayValores = new int[nValores];
-        for (int i = 0; i < ArrayValores.length; i++) {
-            ArrayValores[i] = i + 1;
-        }
-        // COMO QUITO EL ORDEN A LOS ELEMENTOS DE ARRAY.
-        
-        
-
-        // Una vez tengo rellenado el array, procedo a rellenar la matriz con dichos valores. 
-        int Aux = 0;
-        for (int i = 0; i < matrizAux.length; i++) {
-            System.out.println(" ");
-            for (int j = 0; j < matrizAux.length; j++) {
-                if (Aux < ArrayValores.length) {
-                    matrizAux[i][j] = ArrayValores[Aux];
-                    Aux++;
-                } else {
-                    return;
-                }
-            }
-        }
-    }
+//    public static void rellenarMatrizSinRepetirValores(int n, int[][] matrizAux) {
+//        Random random = new Random();
+//        int nValores = (n * n);
+//        //Creo un Array y lo relleno con valores desde (1-n*n).
+//        int[] ArrayValores = new int[nValores];
+//        for (int i = 0; i < ArrayValores.length; i++) {
+//            ArrayValores[i] = i + 1;
+//        }
+//        // COMO QUITO EL ORDEN A LOS ELEMENTOS DE ARRAY.
+//        
+//        
+//
+//        // Una vez tengo rellenado el array, procedo a rellenar la matriz con dichos valores. 
+//        int Aux = 0;
+//        for (int i = 0; i < matrizAux.length; i++) {
+//            System.out.println(" ");
+//            for (int j = 0; j < matrizAux.length; j++) {
+//                if (Aux < ArrayValores.length) {
+//                    matrizAux[i][j] = ArrayValores[Aux];
+//                    Aux++;
+//                } else {
+//                    return;
+//                }
+//            }
+//        }
+//    }
 }
--- src/main/java/repasoestructurasalmacenamiento/Ejercicio2.java
+++ src/main/java/repasoestructurasalmacenamiento/Ejercicio2.java
@@ -15,10 +15,10 @@ public class Ejercicio2 {
 
     public static void main(String[] args) {
 
-        int n = 3;
+        int n = 9;
         int[][] matriz = new int[n][n];
         rellenarMatrizRandom(matriz);
-        System.out.println("DIMENSIÓN:        3");
+        System.out.println("DIMENSIÓN:        " + n);
         System.out.println("            MATRIZ CREADA");
         mostrarMatriz(matriz);
         // LO QUE SIGUE...
@@ -48,7 +48,23 @@ public class Ejercicio2 {
     
     // Método que comprueba las esquinas de aquellas coordenadas donde se encuentra un 0. 
     // si todas tienen 1 ese cero ese es el que buscamos.
-    public static void comprobarEsquinasCeros(){
+    public static void comprobarEsquinasCeros(int [][] matriz){
+        // La i y la j la inicializo en 1, ya que esas filas y columnas nunca van a cumplir la condicion que pide el ejercicio. 
+        for (int i = 1; i < matriz.length; i++) {
+            for (int j = 1; j < matriz[i].length; j++) { // Le pongo matriz[i] para que recorra eso, y no se salga.
+                // Aquí le paso como condición, las posiciones en las que se debe de encontrar un 1.
+                if (
+                           matriz[i][j] ==1 // Cuando ese cero, tiene un 1 en la esquina superior izquierda
+                        && matriz[i][j] ==1 // Cuando ese cero, tiene un 1 en la esquina superior derecha
+                        && matriz[i][j] ==1 // Cuando ese cero, tiene un 1 en la esquina inferior izquierda
+                        && matriz[i][j] ==1 // Cuando ese cero, tiene un 1 en la esquina inferior derecha
+                        ) {
+                    
+                    
+                }
+            }
+        }
+        
         
     }
 
