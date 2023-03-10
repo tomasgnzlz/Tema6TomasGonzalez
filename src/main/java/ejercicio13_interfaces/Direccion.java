7d18f828db3849c8d0bf6936729169f6906ed151
tomasgnzlz <tgonati327@g.educaand.es>
2023-03-10 11:07:07 +0100

    Ejercicio 13 + ExplicaciónInterfaces

--- src/main/java/ejercicio13_interfaces/Direccion.java
+++ src/main/java/ejercicio13_interfaces/Direccion.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Record.java to edit this template
  */
-package ejercicio13;
+package ejercicio13_interfaces;
 
 /**
  *
